ff5145365b0063b2caed139f96136ce4d0f4c4b3
jemluz <contatojemima@gmail.com>
2024-01-14 21:32:48 -0300

    Estrutura visual - Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from '../styles/pages/app'
 
+import Image from "next/image"
+
 // rever aulas
 // Estrutura visual - Configurando Stitches
 // Estrutura visual - Estilos globais
@@ -12,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
