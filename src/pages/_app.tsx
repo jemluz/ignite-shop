db2aa8221705905d07d59400396e831c4374d734
jemluz <contatojemima@gmail.com>
2024-01-14 21:31:09 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from 'next/app'
 
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from '../styles/pages/app'
+
+// rever aulas
+// Estrutura visual - Configurando Stitches
+// Estrutura visual - Estilos globais
+// Estrutura visual - Cabeçalho da aplicação
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
